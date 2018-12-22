d28156494430f2e72098bd21174a6a04c0860c8f
Asmaa Ayyash <asmaaayyash22@gmail.com>
2018-12-22 17:22:25 +0300

    update view admin title

--- public/admins/superadmin/view.php
+++ public/admins/superadmin/view.php
@@ -3,9 +3,9 @@
 <?php require_once(PRIVATE_PATH . '/includes/super_admin_header.php'); ?>
 
 <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
-    <h2 class="w3-text-light-grey">Insert New Admin</h2>
+    <h2 class="w3-text-light-grey">View Admin</h2>
     <hr style="width:200px" class="w3-opacity">
-    <p><font color ='#F8F8F8'>Provide all information below </font></p>
+    <p><font color ='#F8F8F8'> Information of Admin  </font></p>
 
 <?php if (is_get_request()) {
   $admin = Admin::find_by_id($_GET['id']);
