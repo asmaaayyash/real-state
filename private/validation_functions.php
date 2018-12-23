556f5b271bec9b3605bbc9953578b3bd83cddefa
Asmaa Ayyash <asmaaayyash22@gmail.com>
2018-12-23 11:55:49 +0300

    add check validate to name, username, and email to validation functions file

--- private/validation_functions.php
+++ private/validation_functions.php
@@ -1,5 +1,27 @@
 <?php
 
-  
+  public function validate_username($username) {
+    if (!preg_match('/^[a-zA-Z0-9]{5,}$/', $username)) {
+      echo "Username must be 5 characters at least - english chars & numbers only allowed ";
+  }
+}
 
+  public function validate_name($name) {
+    if(strlen($name) < 8){
+      echo " Name must be 8 or more charactes";
+    }
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      echo "Only letters and white space allowed";
+    }
+
+  }
+
+  public function validate_email($email) {
+    if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+      return FALSE;
+    }
+    //check if the domail is real
+    $domain = explode("@", $email, 2);
+    return checkdnsrr($domain[1]); // returns TRUE/FALSE;
+}
  ?>
